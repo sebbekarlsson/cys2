4ae1161897a86f7f90f28ad02d6777ab8b36a569
sebbekarlsson <sebbekarlsson97@gmail.com>
2015-01-20 10:20:49 -0800

    upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<html>
+	<header>
+		<link rel="stylesheet" type="text/css" href="styles/style.css">
+		<link href='http://fonts.googleapis.com/css?family=Cabin+Condensed' rel='stylesheet' type='text/css'>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="utf-8">
+	</header>
+
+	<body align"center">
+		<div class="topborder">
+			<div class="toppart">
+			CLASSIC YACHT SERVICE
+			</div>
+
+			<div class="toppart" align="center">
+				<ul>
+					<li><a class="topbtn">Nyheter</a></li>
+					<li><a class="topbtn">Båtar</a><div class="dropdown">
+						<a class="dropbtn">Båt1</a>
+						<a class="dropbtn">Båt2</a>
+						<a class="dropbtn">Båt3</a>
+						<a class="dropbtn">Båt4</a>
+					</div></li>
+					<li><a class="topbtn">Mer om oss</a><div class="dropdown">
+						<a class="dropbtn">Vilka är vi?</a>
+						<a class="dropbtn">Vad gör vi?</a>
+						<a class="dropbtn">Vart finns vi?</a>
+					</div></li>
+				</ul>
+			</div>
+
+			<div class="toppart">
+				<ul>
+					<li>
+						<form method="get" action="search.php" style="display:inline;">
+						<input class="input" name="q" type="text" placeholder="sökord"/>
+						<input class="topbtn" type="submit" value="Sök"/>
+						</form>
+					</li>
+				</ul>
+				
+			</div>
+		</div>
+
+
+		<div class="bannerholder" style="background-image:url(images/yacht.jpg);">
+			<div class="banner">
+			</div>
+		</div>
+
+		<div class="middlepart" align="center">
+			<div class="content">
+				<div class="container">
+					<div class="newsmenu">
+						<a class="newsbtn">2015 06/07</a>
+						<a class="newsbtn">2015 06/07</a>
+						<a class="newsbtn">2015 06/07</a>
+						<a class="newsbtn">2015 06/07</a>
+						<a class="newsbtn">2015 06/07</a>
+					</div>
+					<div class="newspage" align="left">
+						<div class="text">
+						<h2>Livskvalitet är en vacker träbåt</h2>
+						Hur mår din träbåt? Kanske behöver något bytas eller bara fräschas upp? Kanske finns en dröm om en nybyggd båt vid bryggan? Vi erbjuder lösningar på era funderingar och önskemål. Vad ni än behöver ha gjort så gör vi noggranna arbeten med hög kvalitet. Både traditionella och moderna metoder används för bästa resultat. Classic Yacht Service erbjuder också hållbara och underhållsfria produkter för Din båt. 
+						Välkommen till Classic Yacht Service!
+					</div>
+				<div style="clear:both;"></div>
+				</div>
+				<div style="clear:both;"></div>
+			</div>
+		</div>
+
+	</body>
+
+	<footer>
+	</footer>
+</html>
